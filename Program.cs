939cef976a8298fdbe3d4504a9d692b138539934
FernandoZuchi <nandozuchi07@gmail.com>
2024-02-21 21:42:58 -0300

    Subindo as duas pastas contendo cógidos das duas últimas aulas

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void ImprimeSomaIntervalo(int menor, int maior, int soma)
+{
+    if(menor > maior)
+    {
+        Console.WriteLine(soma);
+        return;
+    }
+
+    soma += menor;
+    ImprimeSomaIntervalo(menor + 1, maior, soma);
+}
+
+ImprimeSomaIntervalo(3, 5, 0)

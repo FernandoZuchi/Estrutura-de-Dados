7e7cac735c57ae38ac4487e8a8b256d68a2a1d65
FernandoZuchi <nandozuchi07@gmail.com>
2024-03-23 16:01:40 -0300

    Soma vetor recursivamente, InsertSort

--- 22_03 - Recursividade e Ordenação (Estudos)/Ordenacao/InsertSort/Program.cs
+++ 22_03 - Recursividade e Ordenação (Estudos)/Ordenacao/InsertSort/Program.cs
@@ -3,10 +3,32 @@
     static void Main(string[] args)
     {
         int [] vet = {2, 1, 5, 9, 3, 4, 8, 6};
+        InsertSort(vet);
+        Imprime(vet);
     }
     // Ordenar um vetor 
-    static void InsertSort()
+    static void InsertSort(int[] vet)
     {
+        // A parte ordenada do vetor está entre 0 e i
+        for (int i = 1; i < vet.Length; i++)
+        {
+            int chave = vet[i]; // Valor a ser inserido na parte ordenada
+            int j = i - 1; // O j irá percorrer do elemento anterior a chave até zero
 
+            // Localizando a posição j de Chave na parte ordenada:
+            while(j >= 0 && chave < vet[j])
+            {
+                vet[j+1] = vet[j]; // Empurrando pro final do vetor elementos > chave
+                j--; // Para percorrer de j até zero
+            }
+            vet[j + 1] = chave; // Inserimos a chave na posição correta do vetor
+        }
+    }
+    static void Imprime(int[] vet)
+    {
+        for (int i = 0; i < vet.Length; i++)
+        {
+            Console.Write($"{vet[i]} ");
+        }
     }
 }
---
+++ 22_03 - Recursividade e Ordenação (Estudos)/Recursividade/SomaVetor/Program.cs
@@ -0,0 +1,13 @@
+﻿// Somar todos elementos do vetor de forma recursiva
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vet = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine(Soma(vet, 0)); 
+    }
+    static int Soma(int[] vet, int i)
+    {    
+        return i == vet.Length ?  0 : Soma(vet, i + 1) + vet[i];
+    }
+}

639eff2093c5684a26df88c7d4c885929cd5ccd9
FernandoZuchi <nandozuchi07@gmail.com>
2024-04-03 21:39:01 -0300

    Questão 1 do simulado feita

---
+++ 03_04 - Simulado Ordenação e Recursividade/questao1/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    // Faça uma função recursiva que recebe um número inteiro por parâmetro e retorne true se o número for um número perfeito e false caso contrário. Um número perfeito é um número natural para o qual a soma de todos os seus divisores naturais próprio é igual ao próprio número
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número inteiro");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int divisor = 1;
+
+        if (Perfeito(num, divisor, 0))
+        {
+            Console.WriteLine($"O número {num} é perfeito!");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} não é perfeito!");
+        }
+    }
+
+    static bool Perfeito(int num, int divisor, int soma)
+    {
+        // Condição de parada, divisor percorreu até o número
+        // Chegando na condição de parada ele retorna se a soma é igual ao número (sim = true) || (não = false)
+        if (divisor == num)
+            return soma == num;
+
+        // Validação recursiva para verificar os divisores do num, se for divisível, acumula na variável soma
+        if (num % divisor == 0)
+            soma += divisor;
+
+        
+        // Chamada recursiva, caso não tenha entrado na condição de parada, chama o procedimento agora passando o próximo divisor como parâmetro
+        return Perfeito(num, divisor + 1, soma);
+
+
+    }
+}
---
+++ 27_03 - Revisão ordenacao_recursividade/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SomaParesEntre(1, 5));
+    }
+    static int SomaParesEntre(int min, int max)
+    {
+        if(min > max)
+            return 0;
+
+        return (min % 2 == 0 ? min : 0) + SomaParesEntre(min + 1, max);
+    }
+    // Uma forma de fazer
+    // static int SomaParesEntre(int min, int max, int soma)
+    // {
+    //     if(min > max)
+    //         return soma;
+
+    //     if(min % 2 == 0)
+    //         soma += min;
+
+    //     return SomaParesEntre(min + 1, max, soma);
+    // }
+
+    // static void Imprime(int qtdeVezes)
+    // {
+    //     if(qtdeVezes == 0)
+    //         return;
+
+    //     Imprime(qtdeVezes - 1);
+
+    //     Console.WriteLine($"{qtdeVezes}) Estude Estrutura de Dados!");
+    // }
+}

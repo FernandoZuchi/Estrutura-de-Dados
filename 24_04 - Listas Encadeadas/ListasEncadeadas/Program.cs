f6ee471183b1e6d959b543ac79487c6ced837495
FernandoZuchi <nandozuchi07@gmail.com>
2024-04-24 21:51:51 -0300

    Listas encadeadas

---
+++ 24_04 - Listas Encadeadas/ListasEncadeadas/Program.cs
@@ -0,0 +1,19 @@
+﻿var lista = new ListaEncadeada();
+
+lista.AdicionarNoInicio(10);
+lista.AdicionarNoInicio(7);
+lista.AdicionarNoInicio(2);
+lista.AdicionarNoFinal(50);
+lista.AdicionarNoFinal(1);
+
+lista.RemoverNoInicio();
+lista.RemoverNoFinal();
+
+var noAtual = lista.Primeiro;
+
+while (noAtual != null)
+{
+    Console.Write($"{noAtual.Valor}  ");
+    noAtual = noAtual.Proximo;
+}
+﻿
